6e8c26b6e450f5fe0c05b8c66427811cfffe85b2
tinhan <tinhanc@gmail.com>
2020-10-26 12:23:22 +0700

    innit code

---
+++ app/build/generated/source/buildConfig/androidTest/debug/tin/todo/clear/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tin.todo.clear.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tin.todo.clear.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ app/src/main/java/tin/todo/clear/MainActivity.java
@@ -0,0 +1,15 @@
+package tin.todo.clear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tin.todo.clear.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
